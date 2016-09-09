8ab5a4c661581c99a75d32bcfb8c25adb1f2ba20
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-09 15:16:11 +0300

    Фигуры Лиссажу

--- 2.py
+++ 2.py
@@ -6,18 +6,18 @@ from matplotlib import mlab
 tmin = -20.0
 tmax = 20.0
 
-tx = 0.01
-tlist = mlab.frange (tmin, tmax, tx)
+dt = 0.01
+tlist = mlab.frange (tmin, tmax, dt)
 
 pylab.ion()
 
-for n in range (500):
-    xlist = [math.sin(t + n/10.0) for t in tlist]
+for n in range (250):
+    xlist = [math.sin(t + n/24.0) for t in tlist]
     ylist = [math.cos (t*2.0) for t in tlist]
     pylab.clf()
     pylab.plot (xlist, ylist)
     pylab.draw()
-    pylab.pause(0.3)
+    pylab.pause(0.001)
 
 
 pylab.close()
