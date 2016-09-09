c1733d6aec48a553566763ea5043e12b23590175
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-09 15:13:10 +0300

    страшный логарифм

---
+++ 4.py
@@ -0,0 +1,6 @@
+__author__ = 'Jan'
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(-10, 10.01, 0.01)
+plt.plot(np.log((x**2+1)*np.exp(-np.abs(x)/10),1+np.tan(1/(1+np.sin(x)**2))))
+plt.show()
